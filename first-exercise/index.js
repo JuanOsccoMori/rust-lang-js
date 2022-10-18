309c5d7c75b2a995ba28d2421c14a182d7843dca
Juan Oscco <juan.osccomori@gmail.com>
2022-10-18 18:47:34 -0500

    Agregando primer proyecto nodejs

---
+++ first-exercise/index.js
@@ -0,0 +1,6 @@
+
+// Mensaje de bienvenida
+let message = "Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo";
+
+// Impresion de mensaje
+console.log(message);
