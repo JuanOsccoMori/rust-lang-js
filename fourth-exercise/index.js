db038475eb15fbbd48ccbaa05c8c2966e417fd37
Juan Oscco <juan.osccomori@gmail.com>
2022-10-23 14:49:06 -0500

    suibiendo ejercicio cuatro

---
+++ fourth-exercise/index.js
@@ -0,0 +1,20 @@
+let name = "Juan Carlos";
+let last_name = "Oscco Mori";
+
+let student = `${name} - ${last_name}`;
+
+let estudianteMayus = student.toUpperCase();
+
+let estudianteMinus = student.toLowerCase();
+
+let letters = student.length;
+
+let letter_first = student.charAt(0);
+
+let letter_end = student.charAt(23);
+
+let letter_space = student.replace(/\s+/g,'');
+
+let name_find = student.includes("Juan Carlos");
+
+console.log(name_find);
