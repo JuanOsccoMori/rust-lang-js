581ab5d6786d89d0d39aedb6e55183ae07296ecc
Juan Oscco <juan.osccomori@gmail.com>
2022-10-23 19:12:54 -0500

    Agregando el ejercicio cinco

---
+++ fifth-exercise/index.js
@@ -0,0 +1,13 @@
+let height = 165
+
+let height_meters = 1.65
+
+let weight = 75.5
+
+let height_max = Math.ceil(height_meters);
+
+let weight_min = Math.floor(weight)
+
+let equals = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
+
+console.log(height, height_meters, height_max,weight, weight_min,equals)
