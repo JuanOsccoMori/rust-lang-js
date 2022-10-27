633d8ba9b5d7a16aa2410fa28f6e44f638b7d245
Juan Oscco <juan.osccomori@gmail.com>
2022-10-26 23:27:24 -0500

    subiendo ejercicio ocho

---
+++ eighth-exercise/conjuntos.js
@@ -0,0 +1,36 @@
+const my_data = {
+    name: "Juan",
+    last_name: "Oscco Mori",
+    age: 24,
+    heigth: 165,
+    isDeveloper: true
+}
+const age = my_data.age;
+
+const circle = [
+    {
+        name: "Juan",
+        last_name: "Oscco Mori",
+        age: 24,
+        heigth: 165,
+        isDeveloper: true
+    },
+    {
+        name: "Hector Alonso",
+        last_name: "Loarte Mori",
+        age: 26,
+        heigth: 161,
+        isDeveloper: false
+    },
+    {
+        name: "Simon",
+        last_name: "Cuesta Mori",
+        age: 21,
+        heigth: 167,
+        isDeveloper: true
+    }
+]
+
+circle.sort((a, b) => b.age - a.age);
+console.log(age);
+console.log(circle);
