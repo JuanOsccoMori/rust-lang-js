ac0b142ce52628f3c1ee1f403bfd9d9e502a6a55
Juan Oscco <juan.osccomori@gmail.com>
2022-10-26 23:11:04 -0500

    subiendo ejercicio siete

---
+++ seventh-exercise/conjuntos.js
@@ -0,0 +1,12 @@
+const family_name = new Set()
+
+
+family_name.add("juan");
+family_name.add("Maria");
+family_name.add("Paco");
+
+console.log(family_name);
+family_name.add("juan");
+console.log(family_name);
+family_name.add("JavasCript");
+console.log(family_name);
