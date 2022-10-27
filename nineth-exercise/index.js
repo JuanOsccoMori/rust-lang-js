8347c4930211653ac24b2cdd311ec4a7fccc11e5
Juan Oscco <juan.osccomori@gmail.com>
2022-10-26 23:37:23 -0500

    subiendo ejercicio nueve

---
+++ nineth-exercise/index.js
@@ -0,0 +1,16 @@
+const date = new Date(2022,09,26);
+
+console.log(date);
+
+const date_my_born = new Date(1998,02,22);
+console.log(date_my_born);
+
+const later = date > date_my_born;
+console.log(later);
+
+const my_day_born = date_my_born.getDate();
+
+const my_month_born = date_my_born.getMonth() + 1;
+const my_year_born = date_my_born.getFullYear();
+
+console.log(my_day_born, my_month_born, my_year_born);
